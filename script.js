2b6505051cbe7a2e01f71fe75267b9ff0e981b13
doniahmad <doniahmaddarmawan@gmail.com>
2021-10-22 10:56:30 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// active nav
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+
+  showMenu("nav-toggle", "navbarNav");
+};
